6c98035d026719a2111e6911cffb9e1940a26aba
Hangeris <liudas.spangelevicius@gmail.com>
2021-12-05 20:03:49 +0200

    part2

--- main.py
+++ main.py
@@ -16,20 +16,18 @@ class Entry:
         self.y2 = int(split_raw_line[1].split(',')[1])
         
         self.points = []
+        
+        x_increment = 1 if self.x1 < self.x2 else -1
         if self.x1 == self.x2:
-            print("{:2}x are equal. range from {:2} to {:2}".format(self.x1, self.y1, self.y2))
-            increment_amount = 1 if self.y1 < self.y2 else -1
-            for y in range(self.y1, self.y2 + increment_amount, increment_amount):
-                self.points.append(Point(self.x1, y))
-
+            x_increment = 0
+            
+        y_increment = 1 if self.y1 < self.y2 else -1
         if self.y1 == self.y2:
-            print("{:2}y are equal. range from {:2} to {:2}".format(self.y1, self.x1, self.x2))
-            increment_amount = 1 if self.x1 < self.x2 else -1
-            for x in range(self.x1, self.x2 + increment_amount, increment_amount):
-                self.points.append(Point(x, self.y1))
-                
-    def is_diagonal(self):
-        return self.x1 != self.x2 and self.y1 != self.y2
+            y_increment = 0
+            
+        increment_amount = max(abs(self.x1 - self.x2), abs(self.y1 - self.y2)) + 1
+        for i in range(0, increment_amount):
+            self.points.append(Point(self.x1 + i * x_increment, self.y1 + i * y_increment))
 
     def show(self):
         print("{:2},{:2} -> {:2},{:2}  has these points v".format(self.x1, self.y1, self.x2, self.y2))
@@ -40,24 +38,25 @@ class Entry:
 def main():
     input_lines = read_file_lines()
     
+    # append all entries with all the points
     entries = []
     for line in input_lines:
         entry = Entry(line)
         entries.append(entry)
         
+    # add unique points with their amounts
     grid = {}
     for entry in entries:
-        if entry.is_diagonal():
-            continue
-            
         for point in entry.points:
             point_key = (point.x, point.y)
             if not grid.__contains__(point_key):
                 grid[point_key] = 0
             grid[point_key] += 1
             
+    # calculate the score
     answer = 0
     for point in grid:
+        print(point)
         if grid[point] >= 2:
             answer += 1
          

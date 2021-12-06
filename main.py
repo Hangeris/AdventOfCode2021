b649781ebd0d3f91122ef8ef8f6d3c78c67365fc
Hangeris <liudas.spangelevicius@gmail.com>
2021-12-06 11:12:50 +0200

    part1

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
+import sys
+
+class LanternFish:
+    def __init__(self, starting_day):
+        self.health = starting_day
+
+    def decrease(self):
+        self.health -= 1
+
 def main():
     input_lines = read_file_lines()
-    for line in input_lines:
-        print(line)
+    
+    fishes = []
+    input_in_days = input_lines[0].split(',')
+    for input_in_day in input_in_days:
+        fishes.append(LanternFish(int(input_in_day)))
+
+    show(fishes)
+    upcoming_fishes = []
+    
+    for i in range(0, 80):
+        for fish in fishes:
+            if fish.health <= 0:
+                upcoming_fishes.append(LanternFish(8))
+                fish.health = 6
+            else:
+                fish.health -= 1
+
+        fishes.extend(upcoming_fishes)
+        upcoming_fishes = []
+        #show(fishes)
+    
+    print(len(fishes))
+
+def show(fishes):
+    for fish in fishes:
+        sys.stdout.write("{},".format(fish.health))
+    print()
+        
 
 def read_file_lines():
     return open('input.txt', 'r', encoding='utf-8-sig').read().splitlines()

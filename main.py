a445372a2da5b6d56b245bda6518d9da9aed75d6
Hangeris <liudas.spangelevicius@gmail.com>
2021-12-07 11:02:32 +0200

    Minor refactoring

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ def main():
     for line in input_lines:
         print(line)
 
+
 def read_file_lines():
     return open('input.txt', 'r', encoding='utf-8-sig').read().splitlines()
+
 
 if __name__ == "__main__":
     main()

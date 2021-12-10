7a1330988400118b5b5f85b650fb13d7097ef02f
Hangeris <liudas.spangelevicius@gmail.com>
2021-12-10 21:58:00 +0200

    part2

--- main.py
+++ main.py
@@ -29,13 +29,13 @@ def get_opposite_symbol(symbol):
 
 def get_corrupted_symbol_score(symbol):
     if symbol == ')':
-        return 3
+        return 1
     if symbol == ']':
-        return 57
+        return 2
     if symbol == '}':
-        return 1197
+        return 3
     if symbol == '>':
-        return 25137
+        return 4
 
     return None
 
@@ -43,12 +43,14 @@ def get_corrupted_symbol_score(symbol):
 def main():
     input_lines = read_file_lines()
 
-    corrupted_symbols = []
+    incomplete_lists = []
+    
 
     for line in input_lines:
         symbols = list(line)
         buffer = []
         print()
+        is_corrupted = False
 
         for sym in symbols:
             closing_symbol = is_closing_symbol(sym)
@@ -62,19 +64,45 @@ def main():
                 if last_sym == opposite_symbol:
                     buffer = buffer[:-1]
                 else:
-                    corrupted_symbols.append(sym)
-                    print("Error on symbol: {}".format(sym))
+                    is_corrupted = True
+                    #corrupted_symbols.append(sym)
+                    #print("Error on symbol: {}".format(sym))
                     break
+                    
+        if len(buffer) != 0 and not is_corrupted:
+            buffer.reverse()
+            swap_symbols(buffer)
+            incomplete_lists.append(buffer)
 
     print()
     print("end of read")
-    answer = 0
-    for corrupted_symbol in corrupted_symbols:
-        score = get_corrupted_symbol_score(corrupted_symbol)
-        answer += score
-        print(corrupted_symbol)
+    print()
+    
+    answers = []
+    for incomplete_list in incomplete_lists:
+        temp_answer = 0
+        print(incomplete_list)
 
-    print(answer)
+        for sym in incomplete_list:
+            symbol_score = get_corrupted_symbol_score(sym)
+            temp_answer = find_temp_score(temp_answer, symbol_score)
+        print("temp answer: {}".format(temp_answer))
+        
+        answers.append(temp_answer)
+
+    answers.sort()
+
+    mid_index = int(len(answers)/2 - 0.5)
+    print(str(answers[mid_index]))
+
+
+def find_temp_score(temp_answer, symbol_score):
+    return temp_answer * 5 + symbol_score
+
+def swap_symbols(buffer):
+    for i in range(0, len(buffer)):
+        buffer[i] = get_opposite_symbol(buffer[i])
+        
 
 
 def read_file_lines():
